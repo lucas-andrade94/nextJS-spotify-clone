e606c82d1391bb1f70b2c687ee5b061159c90d15
lucas-andrade94 <andradelucas94@gmail.com>
2021-12-29 10:43:30 -0700

    Access to playlists

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,6 @@
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
 import {
   HomeIcon,
@@ -8,12 +10,25 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
+import useSpotify from "../hooks/useSpotify";
+import { playlistIdState } from "../atoms/playlistAtom";
 
 function Sidebar() {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
 
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
@@ -54,15 +69,21 @@ function Sidebar() {
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        {/* Playlist */}
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.length > 0 ? (
+          playlists.map((playlist) => (
+            <p
+              key={playlist.id}
+              className="cursor-pointer hover:text-white"
+              onClick={() => setPlaylistId(playlist.id)}
+            >
+              {playlist.name}
+            </p>
+          ))
+        ) : (
+          <button className="flex items-center space-x-2 hover:text-white">
+            <p>Create a new Playlist</p>
+          </button>
+        )}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from "next/head";
 
+import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>Spotify Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-black h-screen overflow-hidden">
+      <main className="flex">
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
 
       <div>{/* Player */}</div>
